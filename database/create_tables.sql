d4f7b685a98dca265df751447f09044a63ac6f50
lagufka <xmat4@yandex.com>
2026-06-12 17:29:14 +0500

    add Dockerfile and tables creation script for DB initialization

---
+++ database/create_tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR NOT NULL,
+    description TEXT,
+    status VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    category VARCHAR,
+    priority VARCHAR NOT NULL
+);
